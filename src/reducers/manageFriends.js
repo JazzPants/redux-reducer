0d6636bc9792dd3134d0d29d422e94f5937e6ada
Leighton <leighton.l14@gmail.com>
2022-03-16 10:00:58 +1100

    Finished! Check slice solution

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,32 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+
+    case "REMOVE_FRIEND":
+      let friendToRemove = state.friends.find(
+        (friend) => friend.id === action.id
+      );
+      return {
+        friends: state.friends.filter((friend) => friend !== friendToRemove),
+      };
+    default:
+      return state;
+  }
 }
+//class solution
+// let indexFriend = state.friends.map((f) => f.id).indexOf(action.id);
+// return {friends:  [
+//   ...state.friends.slice(0, indexFriend),
+//   ...state.friends.slice(indexFriend + 1, -1),
+// ]};
+
+// my solution
+// let friendToRemove = state.friends.find(
+//     (friend) => friend.id === action.id
+//   );
+//   return {
+//     friends: [
+//       ...state.friends.filter((friend) => friend !== friendToRemove),
+//     ],
+//   };
